25ea16b3a574d874d20f264c963eb33f2ce3961a
brendan <brendangooch@gmail.com>
2024-11-21 17:58:02 +0000

    v2.0.0     updated to dynamic-objects v5.0.0, which has substantial changes in functionality     removed public override widthTo()/heightTo() as these methods no longer exist on parent     made updateHitRadius public as client/child will need to call each time width/height updated     updated isHit/isHitPrecise() methods to work with new DynamicRectangle methods     isHit/isHitPrecise() methods now return a number --> distance to center of entity or -1 on miss     tested fully in browser

--- src/screen-entity.ts
+++ src/screen-entity.ts
@@ -22,40 +22,29 @@ export class ScreenEntity extends DynamicRectangle {
         this.zIndex = zIndex;
     }
 
-    public override widthTo(width: number): number {
-        const duration = super.widthTo(width);
-        this.updateHitRadius();
-        return duration;
+    public updateHitRadius(): void {
+        this.hitRadius = ScreenEntity.HIT_RADIUS_SCALE * (Math.max(this.width.current, this.height.current) / 2);
     }
 
-    public override heightTo(height: number): number {
-        const duration = super.heightTo(height);
-        this.updateHitRadius();
-        return duration;
+    public isHit(x: number, y: number): number {
+        const hit = pointInCircle(x, y, this.position.x, this.position.y, this.hitRadius);
+        if (!hit) return -1;
+        return distanceBetween(x, y, this.position.x, this.position.y);
     }
 
-    public isHit(x: number, y: number): number | false {
-        const hit = pointInCircle(x, y, this.current.x, this.current.y, this.hitRadius);
-        if (!hit) return false;
-        return distanceBetween(x, y, this.current.x, this.current.y);
-    }
-
-    public isHitPrecise(x: number, y: number): number | false {
+    public isHitPrecise(x: number, y: number): number {
         const hit = pointInRotatedRectangle(
             x,
             y,
-            this.current.x,
-            this.current.y,
-            this.current.width,
-            this.current.height,
-            this.current.rotation
+            this.position.x,
+            this.position.y,
+            this.width.current,
+            this.height.current,
+            this.rotation.current
         );
-        if (!hit) return false;
-        return distanceBetween(x, y, this.current.x, this.current.y);
+        if (!hit) return -1;
+        return distanceBetween(x, y, this.position.x, this.position.y);
     }
 
-    private updateHitRadius(): void {
-        this.hitRadius = ScreenEntity.HIT_RADIUS_SCALE * (Math.max(this.current.width, this.current.height) / 2);
-    }
 
 }
